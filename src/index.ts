7186d1489c1f6fc03d6dafeca561d57bd71b88f9
bmurf17 <bmurf17@gmail.com>
2022-11-26 15:44:32 -0500

    Create Udate and Read

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import bodyParser from "body-parser";
 import express from "express";
 import pg from "pg";
 
@@ -9,13 +8,43 @@ const pool = new pg.Pool();
 const app = express();
 const port = process.env.PORT || 3333;
 
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.text({ type: "text/html" }));
 
-app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
-  res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+app.put("/:id/book", (req, res) => {
+  const id = req.params["id"];
+  console.log(id);
+  const { title, img, author, pageCount, genre, user, rating } = req.body;
+  pool.query(
+    "Update book SET title = $1, img = $2, author = $3, pagecount = $4, genre = $5, rating = $6 WHERE id = $7",
+    [title, img, author, pageCount, genre, rating, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(201).send(`Updated Row with ID: ${id}`);
+    }
+  );
+});
+
+app.get("/book", async (req, res) => {
+  const { rows } = await pool.query("SELECT * From book");
+  res.json(rows);
+});
+
+app.post("/book", (req, res) => {
+  const { title, img, author, pageCount, genre, user, rating } = req.body;
+  pool.query(
+    "INSERT INTO book (title, img, author, pagecount, genre, rating) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+    [title, img, author, pageCount, genre, rating],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(201).send(`Book added with title: ${title}`);
+    }
+  );
 });
 
 app.listen(port, () => {
---
+++ types/Book.ts
@@ -0,0 +1,11 @@
+export interface Book {
+    id: string;
+    img: string;
+    title: string;
+    author: string;
+    pageCount: number;
+    genre: string;
+    uid: string;
+    dateRead: Date;
+    rating: number;
+}
