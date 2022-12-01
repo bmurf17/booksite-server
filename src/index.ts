a908b922a8cad6a47e1c48beb0afa0b7890212ef
bmurf17 <bmurf17@gmail.com>
2022-12-01 13:49:52 -0500

    Add differnt cors port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import pg from "pg";
 
 const cors = require("cors");
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "http://localhost:5173",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
